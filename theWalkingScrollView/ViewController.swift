0c426006a763d72505fecf1e92e162fe114a1bf7
rmarcha06 <rmarcha06@gmail.com>
2015-10-12 14:43:38 -0500

    actualizacion

--- theWalkingScrollView/ViewController.swift
+++ theWalkingScrollView/ViewController.swift
@@ -9,19 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    let WIDTH: CGFloat = 237
+    let HEIGHT: CGFloat = 415
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        for var x = 1; x <= 5; x++ {
+            let img = UIImage(named: "\(x)")
+            let imgView = UIImageView(image: img)
+            
+            scrollView.addSubview(imgView)
+            
+            imgView.frame = CGRectMake(-WIDTH + (WIDTH * CGFloat(x)), 320, WIDTH, HEIGHT)
+        }
+        scrollView.contentSize = CGSizeMake(WIDTH * 5, scrollView.frame.size.height)
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
